45bb9663d20b69ce8bdb3eaf6e9e8d49d77823ca
Jack Nguyen <79352883+Cuberates@users.noreply.github.com>
2026-09-15 12:12:03 -0700

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void out(std::vector<pair<int,int>> &countRanks) { 
+        for (auto &rank : countRanks) { 
+            std::cout << rank.first << ' ' << rank.second << '\n';
+        }
+    }
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        std::unordered_map<int, int> counts; 
+        for (auto &num : nums) { counts[num]++; }
+
+        std::vector<pair<int,int>> countRanks;  
+        countRanks.reserve(2001u); // At most 2001 pairs
+        for (auto &frequencyCount : counts) { 
+            countRanks.push_back(std::move(frequencyCount));
+        }
+
+        // out(countRanks); 
+
+        auto fewerCount = [&](std::pair<int,int> &a, std::pair<int,int> &b) -> bool { 
+            return a.second >= b.second; 
+        };
+
+        std::sort(countRanks.begin(), countRanks.end(), fewerCount);
+        // std::reverse(countRanks.begin(), countRanks.end());
+        // assert(k <= countRanks.size());
+
+        std::vector<int> mostFreq;
+        for (size_t idx {0}; idx < k; idx++) { 
+            mostFreq.push_back(countRanks[idx].first);
+        } 
+        return mostFreq;
+    }  
+};
